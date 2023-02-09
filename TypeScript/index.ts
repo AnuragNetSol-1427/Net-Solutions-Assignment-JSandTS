c0c76eab5d0ccdf3d23a805683a250c603d1f1ed
AnuragNetSol-1427 <anurag.sharma@netsolutions.com>
2023-02-09 17:09:05 +0530

    typescript question also done

---
+++ TypeScript/index.ts
@@ -0,0 +1,38 @@
+class Person{
+    "name": string;
+    "age": number;
+
+    constructor(x: string, y: number){
+        this.name = x;
+        this.age = y;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
+      }
+}
+
+// const a = new Person;
+// a.greet("Anurag", 19);
+
+
+class Student extends Person {
+  "courses": string[];
+  "enrolled": boolean;
+
+  constructor(name: string, age: number, courses: string[]){
+    super(name, age);
+    // this.name = name;
+    // this.age = age;
+    this.courses = courses;
+  }
+
+  listCourses(): string[] {
+    return this.courses;
+  }
+}
+
+const stu = new Student("Anurag", 19, ["maths", "physics"]);
+stu.greet();
+stu.listCourses();
+console.log(stu.listCourses());
